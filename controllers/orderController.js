e2bbc333b58e78491bf0d435bf05b74c0db47c70
Malithi Karunarathne <mkarunarathne997@gmail.com>
2023-02-17 17:10:43 +0530

    saveQuantityReceived completed

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -113,47 +113,99 @@ export const saveQuantityReceived = async (req, res, next) => {
   try {
     const order = await Order.findById(_id);
     order.status = "DELIVERED";
-    const inventory = await Inventory.findOne({ user: wardUser }); //user wardUser
+    let inventory = await Inventory.findOne({ user: wardUser }); //user wardUser
+    const pharmacyInventory = await Inventory.findOne({ user: pharmacist }); //user pharmacist
+
+    if (!inventory) {
+      // Create a new inventory for that user
+      inventory = await Inventory.create({
+        user: wardUser,
+      });
+    }
+
     for (let i = 0; i < orderItems.length; i++) {
-      const index = inventory.inventory.findIndex(
+      let totalIssued = 0;
+      const orderItemIndex = order.orderItems.findIndex(
         (item) => item.drug.toString() === orderItems[i].drug.toString()
       );
-      if (index !== -1) {
-        const batchIndex = inventory.inventory[index].batch.findIndex(
-          (item) => item.batchNo === orderItems[i].batch.batchNo
-        );
-        if (batchIndex !== -1) {
-          inventory.inventory[index].batch[batchIndex].quantity =
-            inventory.inventory[index].batch[batchIndex].quantity +
-            orderItems[i].quantityReceived;
-        }
-        inventory.inventory[index].quantityInStock =
-          inventory.inventory[index].quantityInStock +
-          orderItems[i].quantityReceived;
-      }
-      console.log(orderItems[i].drug);
-      const orderItemIndex = order.orderItems.findIndex(
+      const inventoryItemIndex = inventory.inventory.findIndex(
+        (item) => item.drug.toString() === orderItems[i].drug.toString()
+      );
+
+      const pharmacyInventoryItemIndex = pharmacyInventory.inventory.findIndex(
         (item) => item.drug.toString() === orderItems[i].drug.toString()
       );
       if (orderItemIndex === -1) {
         throw new Error("order item not found");
       }
-      //update quantityreceived in issuedrugs array
-
       for (let j = 0; j < orderItems[i].issueDrugs.length; j++) {
         const issueDrugIndex = order.orderItems[
           orderItemIndex
         ].issueDrugs.findIndex(
           (item) => item.batch === orderItems[i].issueDrugs[j].batch
         );
+        const pharmacyInventoryBatchIndex = pharmacyInventory.inventory[
+          pharmacyInventoryItemIndex
+        ].batch.findIndex(
+          (item) => item.batchNo === orderItems[i].issueDrugs[j].batch
+        );
+        //expire date of pharmacyInventoryBatchIndex batch
+        const expiredate =
+          pharmacyInventory.inventory[pharmacyInventoryItemIndex].batch[
+            pharmacyInventoryBatchIndex
+          ].expDate;
+
         if (issueDrugIndex === -1) {
           throw new Error("issue drug not found");
         }
         order.orderItems[orderItemIndex].issueDrugs[
           issueDrugIndex
         ].quantityRecieved = orderItems[i].issueDrugs[j].quantityRecieved;
+        totalIssued =
+          totalIssued + orderItems[i].issueDrugs[j].quantityRecieved;
+
+        if (inventoryItemIndex === -1) {
+          //create a new inventory item
+          inventory.inventory.push({
+            drug: orderItems[i].drug,
+            batch: [
+              {
+                batchNo: orderItems[i].issueDrugs[j].batch,
+                quantity: orderItems[i].issueDrugs[j].quantityRecieved,
+                expDate: expiredate,
+              },
+            ],
+            quantityInStock: orderItems[i].issueDrugs[j].quantityRecieved,
+            reorderLevel: 0,
+          });
+        } else {
+          //update existing inventory item
+          const batchIndex = inventory.inventory[
+            inventoryItemIndex
+          ].batch.findIndex(
+            (item) => item.batchNo === orderItems[i].issueDrugs[j].batch
+          );
+          if (batchIndex === -1) {
+            //create a new batch
+            inventory.inventory[inventoryItemIndex].batch.push({
+              batchNo: orderItems[i].issueDrugs[j].batch,
+              quantity: orderItems[i].issueDrugs[j].quantityRecieved,
+              expDate: expiredate,
+            });
+          } else {
+            //update existing batch
+            inventory.inventory[inventoryItemIndex].batch[batchIndex].quantity =
+              inventory.inventory[inventoryItemIndex].batch[batchIndex]
+                .quantity + orderItems[i].issueDrugs[j].quantityRecieved;
+          }
+          inventory.inventory[inventoryItemIndex].quantityInStock =
+            inventory.inventory[inventoryItemIndex].quantityInStock +
+            orderItems[i].issueDrugs[j].quantityRecieved;
+        }
       }
+      order.orderItems[orderItemIndex].totalIssued = totalIssued;
     }
+
     await inventory.save();
     await order.save();
     res.status(200).json({ status: "success", order: order });
