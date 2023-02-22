938bc12475225e51e8b2ebe337889edb460944fa
Malithi Karunarathne <mkarunarathne997@gmail.com>
2023-02-23 01:01:01 +0530

    reports

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -4,7 +4,7 @@ import Order from "../models/Order.js";
 
 //drug issue report
 export const drugIssueReport = async (req, res, next) => {
-  const { pharmacist, month } = req.body;
+  const { pharmacist, month, year } = req.body;
   try {
     console.log("wffg");
     const orders = await Order.aggregate([
@@ -61,11 +61,12 @@ export const drugIssueReport = async (req, res, next) => {
           status: 1,
         },
       },
-      //   // get in this month
+      //   // get in this month and year
       {
         $match: {
           $expr: {
             $eq: [{ $month: "$updatedAt" }, month],
+            $eq: [{ $year: "$updatedAt" }, year],
           },
         },
       },
@@ -159,6 +160,89 @@ export const inventoryReport = async (req, res, next) => {
   } catch (error) {
     console.log(error);
     res.status(400).json({ error: "cannot get inventory data" });
+    next();
+  }
+};
+
+//drug usage chart
+
+export const pharmacyDrugUsageChart = async (req, res, next) => {
+  const { pharmacist, month, year, drug } = req.body;
+
+  try {
+    const orders = await Order.aggregate([
+      {
+        $match: {
+          pharmacist: mongoose.Types.ObjectId(pharmacist),
+        },
+      },
+      {
+        $unwind: "$orderItems",
+      },
+      //get accepted or delivered orders
+      {
+        $match: {
+          $or: [{ status: "DELIVERED" }, { status: "ACCEPTED" }],
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          orderItems: 1,
+          date: 1,
+          updatedAt: 1,
+          status: 1,
+        },
+      },
+      {
+        $match: {
+          "orderItems.drug": mongoose.Types.ObjectId(drug),
+        },
+      },
+      {
+        $project: {
+          quantity: "$orderItems.totalIssued",
+          date: 1,
+          updatedAt: 1,
+          _id: 1,
+        },
+      },
+      //  // get in this month and year
+      {
+        $match: {
+          $expr: {
+            $eq: [{ $month: "$updatedAt" }, month],
+            $eq: [{ $year: "$updatedAt" }, year],
+          },
+        },
+      },
+      //group by date
+      {
+        $group: {
+          _id: {
+            date: { $dateToString: { format: "%Y-%m-%d", date: "$updatedAt" } },
+          },
+          total: { $sum: "$quantity" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          date: "$_id.date",
+          total: 1,
+        },
+      },
+      {
+        $sort: {
+          date: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({ status: "success", usage: orders });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "cannot get orders" });
     next();
   }
 };
--- routes/reportsRoutes.js
+++ routes/reportsRoutes.js
@@ -3,11 +3,13 @@ import { Router } from "express";
 import {
   drugIssueReport,
   inventoryReport,
+  pharmacyDrugUsageChart,
 } from "../controllers/reportsController.js";
 
 const router = Router();
 
 router.post("/drugIssueReport", drugIssueReport);
 router.post("/inventoryReport", inventoryReport);
+router.post("/pharmacyDrugUsageChart", pharmacyDrugUsageChart);
 
 export default router;
