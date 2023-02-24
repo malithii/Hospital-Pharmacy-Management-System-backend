bd0056c29bb0e0ce6bbf9460d891b9ebca140708
Malithi Karunarathne <mkarunarathne997@gmail.com>
2023-02-24 23:58:54 +0530

    total drugs usage chart

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -246,3 +246,81 @@ export const pharmacyDrugUsageChart = async (req, res, next) => {
     next();
   }
 };
+
+// total drugs usage chart
+
+export const pharmacyTotalDrugUsageChart = async (req, res, next) => {
+  const { pharmacist, month, year } = req.body;
+
+  try {
+    const orders = await Order.aggregate([
+      {
+        $match: {
+          pharmacist: mongoose.Types.ObjectId(pharmacist),
+        },
+      },
+      {
+        $unwind: "$orderItems",
+      },
+      //get accepted or delivered orders
+      {
+        $match: {
+          $or: [{ status: "DELIVERED" }, { status: "ACCEPTED" }],
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          orderItems: 1,
+          date: 1,
+          updatedAt: 1,
+          status: 1,
+        },
+      },
+      {
+        $project: {
+          quantity: "$orderItems.totalIssued",
+          date: 1,
+          updatedAt: 1,
+          _id: 1,
+        },
+      },
+      //  // get in this month and year
+      {
+        $match: {
+          $expr: {
+            $eq: [{ $month: "$updatedAt" }, month],
+            $eq: [{ $year: "$updatedAt" }, year],
+          },
+        },
+      },
+      //group by date
+      {
+        $group: {
+          _id: {
+            date: { $dateToString: { format: "%Y-%m-%d", date: "$updatedAt" } },
+          },
+          total: { $sum: "$quantity" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          date: "$_id.date",
+          total: 1,
+        },
+      },
+      {
+        $sort: {
+          date: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({ status: "success", usage: orders });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "cannot get orders" });
+    next();
+  }
+};
